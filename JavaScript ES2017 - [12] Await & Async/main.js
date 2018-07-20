80d5eea492e361a83cd0e245795de8a05e219a61
noberk <noberkli@gmail.com>
2018-07-20 15:10:38 +0800

    全部使用js导入资源

--- JavaScript ES2017 - [12] Await & Async/main.js
+++ JavaScript ES2017 - [12] Await & Async/main.js
@@ -1,10 +1,16 @@
+//创建
 const TABLEDATA = [
     { name: 'wang', age: 18, address: "nanjing" },
     { name: 'jg', age: 28, address: "nanjing" },
     { name: 'zbbx', age: 38, address: "nanjing" },
     { name: 'rw', age: 18, address: "nanjing" }
 ]
-
+const TABLEDATA2 = [
+    { name: 'wang', age: 99, address: "nanjing" },
+    { name: 'jg', age: 585, address: "nanjing" },
+    { name: 'zbbx', age: 38, address: "nanjing" },
+    { name: 'rw', age: 18, address: "nanjing" }
+]
 
 class Template {
     constructor() {
@@ -13,7 +19,7 @@ class Template {
 
     createTableStrings(data) {
         var tableContent = '';
-        tableContent += `<table>`
+        tableContent += `<table class="table table-striped">`
         data.forEach(row => {
             tableContent += `<tr>`
             for (const key in row) {
@@ -27,15 +33,30 @@ class Template {
 
     }
     createButton(val) {
-        let button = `<button>${val}</button>`;
+        let button = `<button class="btn">${val}</button>`;
         this.combine(button);
     }
+
     combine(htmlPartial) {
         this.HTML += htmlPartial;
     }
 
 }
-class Table {
+
+const Component = (inventory) => {
+
+    let head = document.getElementsByTagName('head')[0];
+
+    inventory.styleUrls.forEach(cssUrl => {
+        let link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = cssUrl;
+        head.appendChild(link);
+    })
+
+}
+class Canvas {
     constructor(id, template) {
         this.template = template;
         let box = this.findWrapper(id);
@@ -53,12 +74,16 @@ class Table {
     }
 
     init(box) {
-        this.template.createTableStrings(TABLEDATA);
-        this.template.createButton('more');
+        this.template.createTableStrings(TABLEDATA)
+        this.template.createTableStrings(TABLEDATA2)
+        this.template.createButton`more`;
+        this.template.createButton`mores`;
+        this.template.createButton`mores`;
         box.innerHTML = this.template.HTML;
     }
 
 
 }
 
-new Table('list', new Template);
+Component({styleUrls:['./bootstrap/css/bootstrap.css']})
+new Canvas('list', new Template);
